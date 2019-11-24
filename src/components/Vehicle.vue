d5c8fab16ab57e3912862101ade5d6635d734954
Dante Castillo <dante.castillo@inallmedia.com>
2019-11-24 16:10:55 -0600

    trying...

--- src/components/Vehicle.vue
+++ src/components/Vehicle.vue
@@ -1,5 +1,39 @@
 <template>
   <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+    <!-- <b-card>
+      <g-image :src="imageSrc" class="card-img-top" alt="..." />
+      <b-card-body>
+        <h3 class="card-title">{{vehicle.Model}}</h3>
+
+        <div class="card-text-promo">
+          <div class="price-text">{{formatPrice(vehicle.NormalPrice)}}</div>
+          <small class="val-text">EUR (netto)</small>
+        </div>
+        <div class="card-list">
+          <div class="card-list_item">
+            <img class="icon" src="/assets/images/truck.svg" />
+
+            <p class="text-list">{{vehicle.VehicleType}}</p>
+          </div>
+          <div class="card-list_item">
+            <img class="icon" src="/assets/images/Enginee.svg" />
+            <p class="text-list">{{vehicle.EnginePower}}</p>
+          </div>
+          <div class="card-list_item">
+            <img class="icon" src="/assets/images/calendar.svg" />
+            <p class="text-list">{{vehicle.RegistrationDate}}</p>
+          </div>
+          <div class="card-list_item">
+            <img class="icon" src="/assets/images/calendar.svg" />
+            <p class="text-list">{{vehicle.Mileage}}</p>
+          </div>
+          <div class="card-list_item">
+            <img class="icon" src="/assets/images/location.svg" />
+            <p class="text-list">{{vehicle.Location}}</p>
+          </div>
+        </div>
+      </b-card-body>
+    </b-card> -->
     <div class="card">
       <g-image :src="imageSrc" class="card-img-top" alt="..." />
       <div class="card-body">
@@ -37,9 +71,12 @@
 </template>
 
 <script>
-
+import { BCard } from "bootstrap-vue";
 export default {
- 
+  components: {
+    BCard
+  },
+
   props: {
     vehicle: {
       type: Object,
@@ -49,14 +86,14 @@ export default {
   computed: {
     imageSrc: function() {
       return `assets/images/${this.vehicle.Image}.jpg`;
-    }    
+    }
   },
   methods: {
     formatPrice(value) {
-        let val = (value/1).toFixed(2).replace('.', ',')
-        return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+      let val = (value / 1).toFixed(2).replace(".", ",");
+      return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     }
-}
+  }
 };
 </script>
 
