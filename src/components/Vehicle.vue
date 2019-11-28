d4fd0841485e29aa63e9a116f977fa03879ef476
dantedubon <dante.dubon@gmail.com>
2019-11-28 12:55:46 -0600

    cosaaaa

--- src/components/Vehicle.vue
+++ src/components/Vehicle.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-lg-3 col-md-6 col-sm-6 col-12 mt-5">    
     <div class="card h-100 ">
-      <a :href="vehicle.Link" target="_blank"><img :src="imageSrc" class="card-img-top"  /></a>
+     <img :src="imageSrc" class="card-img-top"/>
       
       <div class="card-body">
         <h3 class="card-title">{{vehicle.Model}}</h3>
